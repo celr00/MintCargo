33c01bfc1ff398f66c0fda78251fd44c5ee7174f
Carlos Lozada <36938844+celr00@users.noreply.github.com>
2022-11-17 12:46:59 -0600

    Update gulpfile.js
    Gulp file will now save files in the structure that flask needs them to be in

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ function css() {
         .pipe(postcss([autoprefixer(), cssnano()]))
         // .pipe(postcss([autoprefixer()]))
         .pipe(sourcemaps.write('.'))
-        .pipe(dest('build/css'));
+        .pipe(dest('build/static/css'));
 }
 
 function javascript() {
@@ -36,20 +36,20 @@ function javascript() {
       .pipe(terser())
       .pipe(sourcemaps.write('.'))
       .pipe(rename({ suffix: '.min' }))
-      .pipe(dest('./build/js'))
+      .pipe(dest('./build/static/js'))
 }
 
 function imagenes() {
     return src(paths.imagenes)
         .pipe(cache(imagemin({ optimizationLevel: 3 })))
-        .pipe(dest('build/img'))
+        .pipe(dest('build/static/img'))
         .pipe(notify({ message: 'Imagen Completada' }));
 }
 
 function versionWebp() {
     return src(paths.imagenes)
         .pipe(webp())
-        .pipe(dest('build/img'))
+        .pipe(dest('build/static/img'))
         .pipe(notify({ message: 'Imagen Completada' }));
 }
 
