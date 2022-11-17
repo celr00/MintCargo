a8d5f62aaa85380080c2a171ac39d69730afecc7
Eduardo Flores <eduardo.floresramones@gmail.com>
2022-11-17 15:07:57 -0600

    Agregar ruta contact

--- build/app.py
+++ build/app.py
@@ -10,5 +10,9 @@ def index():
 def about():
     return render_template('about-us.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
