fb3a262bebd21b0397fb2d029e5aab549c5dbeb7
Carlos Lozada <36938844+celr00@users.noreply.github.com>
2022-11-17 12:58:43 -0600

    Moving privacy.html to out Flask App

--- build/app.py
+++ build/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def inicio():
     return render_template('index.html')
 
+@app.route('privacy')
+def privacy():
+    return render_template('privacy.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
