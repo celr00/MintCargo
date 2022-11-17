ecb38d31e0ac92e7e491e9d76a903975ed189423
Eduardo Flores <eduardo.floresramones@gmail.com>
2022-11-17 15:05:15 -0600

    Agregar ruta de about us

--- build/app.py
+++ build/app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def inicio():
+def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about-us.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
