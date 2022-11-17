6810a4106bc89e2e3aeafa4f2fc6435e257f4c0e
Eduardo Flores <eduardo.floresramones@gmail.com>
2022-11-17 15:10:26 -0600

    Agregar ruta de services

--- build/app.py
+++ build/app.py
@@ -10,6 +10,10 @@ def index():
 def about():
     return render_template('about-us.html')
 
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
