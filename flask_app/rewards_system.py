fee1638ddb47156fd4247e4d702af9315a705ccd
Eduardo Flores <eduardo.floresramones@gmail.com>
2022-11-24 15:24:52 -0600

    Get orders data

--- flask_app/rewards_system.py
+++ flask_app/rewards_system.py
@@ -14,13 +14,17 @@ def rewards():
     cursor.execute('SELECT * FROM products')
     product_data = cursor.fetchall()
 
+    # Get orders data
+    cursor.execute('SELECT created_at, address_line1, SUM(points_spent), status FROM orders NATURAL JOIN order_details NATURAL JOIN addresses WHERE orders.company_id = %s GROUP BY orders.order_id;' % session['id'])
+    orders_data = cursor.fetchall()
+
     # Get addresses data
     cursor.execute('SELECT * FROM addresses WHERE company_id = %s;' % session['id'])
     addresses_data = cursor.fetchall()
 
     cursor.close()
 
-    return render_template('rewards.html', products=product_data, addresses=addresses_data)
+    return render_template('rewards.html', products=product_data, orders=orders_data, addresses=addresses_data)
 
 @app.route('/update-user', methods=['GET', 'POST'])
 def update_user():
