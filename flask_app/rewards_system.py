2a3aa1af4356776bb597f887e6231141999d41f0
Eduardo Flores <eduardo.floresramones@gmail.com>
2022-11-24 10:12:17 -0600

    Confirm login in rewards

--- flask_app/rewards_system.py
+++ flask_app/rewards_system.py
@@ -1,8 +1,12 @@
 from flask_app import app, mysql
-from flask import render_template
+from flask import render_template, redirect, session
 
 @app.route('/rewards')
 def rewards():
+
+    # Confirm login
+    if not session.get('loggedin') or not session['loggedin']:
+        return redirect('/login')
 
     cursor = mysql.connection.cursor()
 
